6bff2abebc2a24587656f17b1e39af8552135fae
BrainiaK <91491844+BrainiaK1911@users.noreply.github.com>
2021-12-01 01:54:51 -0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 from app import *
+from student import *
 import time
 import numpy as np
 import sys
@@ -6,4 +7,9 @@ import sys
 
 
 if __name__ == '__main__':
-    App().run()
+    #Create Student
+    Raja = Student('Raja', 'Phytogaia', ['Water Whip', 'Bubble', 'Wave', 'Water Drill'], 3, {'INT':3, 'STR':3, 'SPD':3, 'DUR':4, 'EGP':3, 'FHT':5})
+    
+    Gia = Student('Gia', 'Phytogaia', ['Earth Gauntlet', 'Rock Armor', 'Earthquake', 'Sand Spout'], 3, {'INT':3, 'STR':5, 'SPD':3, 'DUR':5, 'EGP':4, 'FHT':3})
+    App().run(Raja, Gia)
+    
